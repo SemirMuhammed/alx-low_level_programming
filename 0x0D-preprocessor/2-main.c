93796d11e761632a7bb8772e5ff96fddab329b64
SemirMuhammed <semirmustafa22@gmail.com>
2024-01-22 17:20:49 +0300

    Project 0x0D - Initial Commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define FILENAME __FILE__
+
+/**
+ * main - Prints the name of the file
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s\n", FILENAME);
+	return (0);
+}
