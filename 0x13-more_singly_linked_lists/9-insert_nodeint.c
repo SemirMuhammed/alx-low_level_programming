fafac4ab521ad9a54743e077cec3abc9335e9167
SemirMuhammed <semirmustafa22@gmail.com>
2024-02-06 18:01:33 +0300

    fix: Adjust condition - Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		while (idx)
 		{
-			if (current == NULL)
+			if (current->next == NULL)
 				return (NULL);
 			previous = current;
 			current = current->next;
