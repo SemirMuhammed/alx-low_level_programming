fea8600a15935421d9e702c4252f407c4077311a
SemirMuhammed <semirmustafa22@gmail.com>
2024-02-07 21:34:08 +0300

    fix: Modify condition inside while loop - Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		while (idx)
 		{
-			if (current->next == NULL && current == NULL)
+			if (!current)
 			{
 				free(new_node);
 				return (NULL);
